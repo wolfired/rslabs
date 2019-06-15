f4d27218b7d2b822111c42a78a0a2e6832ada9ba
LinkWu <wjianl@qq.com>
2019-06-15 13:59:23 +0000

    一些笔记

--- src/bin/lesson001.rs
+++ src/bin/lesson001.rs
@@ -1,5 +1,127 @@
-use rslabs::math;
-
 fn main() {
-    println!("hello {}", math::max(1, 202));
+    {
+        let _x:i32; // 使用let声明变量, 其值在初始化后只读
+        _x = 0; // 初始化
+        // _x = 1; // error code
+
+        let _x = 0; // 使用let声明并初始化变量, 其值只读
+        // _x = 1; // error code
+
+        let mut _x = 0; // 使用let mut声明并初始化变量, 其值可写
+        _x = 1;
+    }
+
+    {
+        const _X: i32 = 0; // 使用const声明常量, 必须同时提供常量数据类型, 必须在声明的同时初始化常量值
+                          // 只接受常量表达式作为右值, 也就是说函数调用返回值或其它运行时运算值均非法
+    }
+
+    {
+        let _x = 0;
+        let _x = 0.0; // 变量隐藏(Shadow), 常量不能隐藏
+
+        let _x = 0;
+        let mut _x = 0.0;
+        _x = 1.0;
+
+        let mut _x = 0;
+        _x = 1;
+        let _x = 0.0;
+    }
+
+    {
+        {
+            let _v_i8 = 0i8; // 8位有符号整型
+            let _v_u8 = 0u8; // 8位无符号整型
+
+            let _v_i16 = 0i16; // 16位有符号整型
+            let _v_u16 = 0u16; // 16位无符号整型
+
+            let _v_i32 = 0; // 32位有符号整型, 默认类型
+            let _v_u32 = 0u32; // 32位无符号整型
+
+            let _v_i64 = 0i64; // 64位有符号整型
+            let _v_u64 = 0u64; // 64位无符号整型
+
+            let _v_isize = 0isize; // 根据系统架构定
+            let _v_usize = 0usize; // 根据系统架构定
+
+            let _v_integer = 1_024; // 十进制(默认), _作为虚拟分隔符方便阅读
+            let _v_integer = 0xFF; // 十六进制
+            let _v_integer = 0o77; // 八进制
+            let _v_integer = 0b1111_0000; // 二进制
+            let _v_integer = b'F'; // 字节, 上面4种写法都可以在值后写上类型以指定变量类型, 而这种写法限制是u8
+        }
+
+        {
+            let _v_f32 = 0.0f32; // 32浮点数
+            let _v_f64 = 0.0; // 64浮点数, 默认类型
+        }
+
+        let _v_bool = true; // 布尔类型
+
+        let _v_char = '\0'; // 字符类型, 使用单引号
+
+        { // 复合数据类型: 元组, 元素数据类型和元素数量都是在编译期就能确定的
+            let _v_tuple = (); // (匿名)(空)(单位)元组类型
+
+            let mut _v_tuple_t0 = (0, 0.0, true);
+            let _v_tuple_t1 = (1, 1.0, false);
+            _v_tuple_t0 = _v_tuple_t1; // 元组类型元素一一对应即认为两个元组类型相同
+
+            let mut _v_tuple = (0); // 当元组数据类型只有一个元素时, 元组数据类型将退化成一个普通数据类型
+            _v_tuple = 1;
+
+            let _v_tuple = (0, 0.0, false, '\0');
+            let (_x, _y, _z, _w) = _v_tuple;
+            let _x = _v_tuple.0;
+            let _y = _v_tuple.1;
+            let _z = _v_tuple.2;
+            let _w = _v_tuple.3;
+        }
+
+        { // 复合数据类型: 数组, 元素数据类型和元素数量都是在编译期就能确定的
+            let _v_array: [i32; 0] = []; //空数组类型
+            let _v_array = [true; 2]; // [true, true]
+        }
+
+        println!("{}", max(0, 1));
+
+        let v_tuple = (5, 3);
+        println!("{}", order_a_tuple(v_tuple).0);
+        println!("{}", v_tuple.0);
+
+        let v_array = [5, 2];
+        println!("{}", order_a_array(v_array)[0]);
+        println!("{}", v_array[0]);
+    }
+}
+
+// 使用 fn 定义一个函数, -> 指定返回值类型, /// 用于文档注释, // 用于一般注释
+/// max 返回两个值中较大那个
+fn max(x: i32, y: i32) -> i32 {
+    if x > y {
+        x //不写 ; 表明这是一个表达式
+    } else {
+        y
+    }
+}
+
+// 使用元组作为返回值类型实现返回多个值
+/// order_a_tuple 从小到大排序一个拥有两个i32元素的元组并返回
+fn order_a_tuple((x, y): (i32, i32)) -> (i32, i32) {
+    if x < y {
+        (x, y)
+    } else {
+        (y, x)
+    }
+}
+
+/// order_a_array 从小到大排序一个拥有两个i32元素的数组并返回
+fn order_a_array(array: [i32; 2]) -> [i32; 2] {
+    if array[0] < array[1] {
+        array
+    } else {
+        [array[1], array[0]]
+    }
 }
--- src/bin/lesson002.rs
+++ src/bin/lesson002.rs
@@ -1,5 +1,137 @@
-use rslabs::math;
+struct S {
+    value: i32,
+}
+
+enum E {
+    Value(i32),
+}
 
 fn main() {
-    println!("world {}", math::min(1, 202));
+    // 所有权规则:
+    // 1. Rust里的每个value都有一个被称为owner的变量
+    // 2. 每个value在一个时间点只能有一个owner
+    // 3. 当owner离开作用域, value将会被删除
+
+    // 一个数据类型的一个实际值所占用的内存, 只有两种情况, 要么全在栈上, 要么部分在栈上部分在堆里
+    // 对一个值进行复制, 仅复制栈上的那部分被称为浅复制, 同时把堆里那部分也复制了被称为深复制
+    // 对于一个全在栈上的值, 浅复制与深复制是一样的, 复制后是完全独立的两个值
+    // 对于一个部分在栈上部分在堆里的值, 浅复制后，栈上那部分值是完全独立的而堆里那部分数据共享, 深复制才是完全独立的两个值
+
+    // Rust默认只进行浅复制, 而且在复制后把源值标记为无效, 所以Rust中浅复制也被称为移动
+
+    // Rust有一个叫Copy的特征(trait), 所有实现这个特征的类型在其进行值复制时都是深复制, Copy特征与Drop特征互斥
+    // 标量(简单)数据类型都是Copy的, 包括: 整型, 浮点型, 布尔型, 字符型
+    // 自定义数据类型: 枚举和结构体, 不是Copy的
+    // 对于复合数据类型: 元组和数组, 只要元素都是Copy的, 那就是Copy的
+    // PS: 在我的理解里, 引用也是Copy的
+
+    // 值的移动发生在三个地方: 赋值, 函数/方法传参, 函数/方法返回值
+
+    // 引用是一种数据类型, 它指向一个值而不获取该值的所有权
+
+    // 使用引用的编程方式被称为借用
+
+    // 引用规则:
+    // 1. 在任意时间点, 在同一作用域内, 要么只有一个可写引用, 要么只有多个只读引用
+    // 2. 引用必须有效
+
+    let _v_t = (32, 32);
+    println!("{}", exchange_tuple(_v_t).0);
+    println!("{}", _v_t.0);
+
+    let _v_a = [32, 32];
+    println!("{}", exchange_array(_v_a)[0]);
+    println!("{}", _v_a[0]);
+
+    {
+        let s = S { value: 32 };
+        exchange_struct(s);
+        // println!("{}", s.value); // error code
+    }
+
+    {
+        let e = E::Value(32);
+        exchange_enum(e);
+        // match e {  // error code
+        //     E::Value(value) => {
+        //         println!("{}", value);
+        //     }
+        // }
+    }
+
+    {
+        let s_ref = &S { value: 32 }; // 使用&获取一个值的引用
+        println!("{}", s_ref.value);
+
+        exchange_struct_ref(s_ref); // 借用
+
+        {
+            let s = S { value: 32 }; // 只读值
+            let s_ref = &s; // 只读引用
+            println!("{} {}", s.value, s_ref.value);
+
+            let mut s = S { value: 32 }; // 可写值
+            s.value = 64;
+            let s_ref = &s; // 只读引用
+            println!("{} {}", s.value, s_ref.value);
+        }
+
+        {
+            let mut _v_s = S { value: 32 }; // 只有可写值, 才可以获取可写引用
+            let _v_s_ref0 = &mut _v_s;
+            // let _v_s_ref1 = &mut s; // error code: 同一作用域内, 对同一个值最多只能有一个可写引用
+            // println!("{} {}", _v_s_ref0.value, _v_s_ref1.value);
+
+            let mut _v_i32 = 0;
+            let _v_i32_ref0 = &mut _v_i32;
+            // let _v_i32_ref1 = &mut _v_i32; // error code
+            // println!("{} {}", _v_i32_ref0, _v_i32_ref1);
+
+            {
+                let _v_i32_ref0: &i32;
+
+                {
+                    let _v_i32 = 0;
+                    // _v_i32_ref0 = &_v_i32; // error code: 引用无效了
+                }
+
+                // println!("{}", _v_i32_ref0); // error code: 未初始化的值不能使用
+            }
+        }
+
+        {
+            let mut _v_s = S { value: 32 };
+            let _v_s_ref0 = &_v_s; // 同一作用域内, 只读引用可以有任意个
+            let _v_s_ref1 = &_v_s;
+            // let _v_s_ref2 = &mut s; // error code: 但同时存在可写引用
+            // println!("{} {} {}", _v_s_ref0.value, _v_s_ref1.value, _v_s_ref2.value);
+        }
+
+        {
+            let _v_a = [0; 5];
+            let _v_s = &_v_a[..];
+
+            let _v_str = String::from("hello world");
+            let _v_s = &_v_str[..];
+        }
+    }
+}
+
+fn exchange_tuple(t: (i32, i32)) -> (i32, i32) {
+    t
+}
+
+fn exchange_array(a: [i32; 2]) -> [i32; 2] {
+    a
+}
+
+fn exchange_struct(s: S) -> S {
+    s
+}
+
+#[allow(unused)]
+fn exchange_struct_ref(s: &S) {}
+
+fn exchange_enum(e: E) -> E {
+    e
 }
